3cbceb1bf448f03f8c0e15b864452f44758a16bf
theburningmonk <theburningmonk@gmail.com>
2019-08-26 02:33:26 +0200

    feat: show callback workflow proxy through sqs

--- Nested workflows/handler.js
+++ Nested workflows/handler.js
@@ -1,11 +1,14 @@
 const AWS = require('aws-sdk')
 const SFN = new AWS.StepFunctions()
 
-module.exports.hello = async event => {
+module.exports.sqs = async event => {
   console.log(JSON.stringify(event))
+
+  const record = event.Records[0]
+  const body = JSON.parse(record.body)
 
   await SFN.sendTaskSuccess({
     output: "42",
-    taskToken: event.Token
+    taskToken: body.Token
   }).promise()
 }
